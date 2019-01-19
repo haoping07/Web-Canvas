6db8d0ffd0778fea1663f99d5c02a25a3bcfbaec
Ping Lin <46828728+pinglin07@users.noreply.github.com>
2019-01-18 20:48:40 -0500

    Update comment

--- myjs.js
+++ myjs.js
@@ -7,37 +7,37 @@ var imgPtr = -1;
 var brushType = 0;
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var att = document.createAttribute("class");    // 更換鼠標用
+var att = document.createAttribute("class");    //Create attribute for cursor changing
 var prevColor = myColor;
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-// 預設畫筆規格
+//Default settings for brush
 ctx.lineWidth = 5;
 ctx.lineJoin = "round";
 ctx.strokeStyle = "grey";
 ctx.fillStyle = ctx.strokeStyle;
 
-// 滑鼠在Canvas中點住時反應
+//Mouse button down event when cursor is inside the canvas
 function mousedown(event) {
     paint = 1;
 
-    // 紀錄按鍵當下的座標 (開始畫畫瞬間的那一點)
+    //Save the cursor position when clicking
     var findposX = event.pageX - canvas.offsetLeft;
     var findposY = event.pageY - canvas.offsetTop;
     addpos(findposX, findposY);
 
-    // 拍照(未畫前)(Un/ReDo用)
+    //Save current canvas content (for Un/Re-do)
     if (imgData.length == 0) {
         imgPtr++;
         imgData[imgPtr] = ctx.getImageData(0, 0, 800, 600);
     }
 
-    // 畫初始點
+    //Start drawing
     PaintStart();
 }
 
-// 滑鼠在Canvas中移動時反應
+//Mouse hovering event
 function mousemove(event) {
     if (paint == 1) {
         var findposX = event.pageX - canvas.offsetLeft;
@@ -47,39 +47,39 @@ function mousemove(event) {
     }
 }
 
-// 滑鼠在Canvas中鬆開的反應
+//Mouse button release event
 function mouseup(event) {
     paint = 0;
     saveposX.pop();
     saveposY.pop();
 
-    // 拍照(Un/ReDo用)
+    //Save current canvas content (for Un/Re-do)
     imgPtr++;
     imgData[imgPtr] = ctx.getImageData(0, 0, 800, 600);
     imgData.splice(imgPtr + 1, imgData.length - imgPtr + 1);
 }
 
-// 滑鼠在Canvas中點住時移出Canvas的反應
+//Mouse button release event when cursor is outside the canvas
 function mouseout() {
     paint = 0;
     saveposX.pop();
     saveposY.pop();
 }
 
-// 鼠標座標儲存功能
+//Save the cursor position
 function addpos(findposX, findposY) {
     saveposX.push(findposX);
     saveposY.push(findposY);
 }
 
-// 畫畫功能
+//Drawing
 function PaintStart() {
     if (eraser_flag != 1)
         changeColor();
 
     ctx.beginPath();
 
-    // 畫點
+    //Draw dot
     if (saveposX.length == 1) {
         ctx.moveTo(saveposX[0] + 1, saveposY[0]);
         ctx.lineTo(saveposX[0], saveposY[0]);
@@ -89,7 +89,7 @@ function PaintStart() {
     else {
         switch (brushType) {
 
-            // 畫線
+            //Draw line
             case 0:
                 ctx.moveTo(saveposX[0], saveposY[0]);
                 ctx.lineTo(saveposX[1], saveposY[1]);
@@ -99,14 +99,14 @@ function PaintStart() {
                 saveposY[0] = saveposY[1];
                 break;
 
-            // 畫圓形
+            //Draw circle
             case 1:
                 ctx.arc(saveposX[0], saveposY[0], circle_radius() * 7, 0, 2 * Math.PI);
                 ctx.stroke();
                 ctx.fill();
                 break;
 
-            // 畫正方形
+            //Draw rectangle
             case 2:
                 ctx.moveTo(saveposX[0], saveposY[0]);
                 ctx.lineTo(saveposX[0] + saveposX[1] - saveposX[0], saveposY[0]);
@@ -117,7 +117,7 @@ function PaintStart() {
                 ctx.fill();
                 break;
 
-            // 畫三角形
+            //Draw triangle
             case 3:
                 ctx.moveTo(saveposX[0], saveposY[0]);
                 ctx.lineTo(saveposX[0] + (saveposX[1] - saveposX[0]) * 1.3, saveposY[0] + (saveposX[1] - saveposX[0]) * 1.6);
@@ -139,7 +139,7 @@ function circle_radius() {
     return Math.sqrt(abs_value);
 }
 
-// 橡皮擦功能
+//Eraser tool
 function eraserTool() {
     brushType = 0;
     eraser_flag = 1;
@@ -149,7 +149,7 @@ function eraserTool() {
     ctx.strokeStyle = "white";
 }
 
-// 筆刷顏色設定 (點擊介面顏色選擇按鈕後執行)
+//Change brush color
 function changeColor() {
     var myColor = document.getElementById("myColor").value;
     ctx.strokeStyle = myColor;
@@ -158,20 +158,20 @@ function changeColor() {
  //   canvas.setAttributeNode(att);
 }
 
-// 筆刷大小設定
+//Change brush size
 function brushSize() {
     var myBrushSize = prompt("Brush Size: ", "5");
     ctx.lineWidth = myBrushSize;
 }
 
-// 新增文字功能
+//Add txt to canvas
 function addTxt() {
     var myTxt = prompt("Add Text Content: ", "write in here...");
-    var myFontSize = prompt("請輸入字體大小(pixel): ", "30");
-    var myFontStyle = prompt("請輸入字型代號: (0: Arial  1: sans-serif  2: serif  3: Verdana)", "0");
-    var myFontIta = prompt("是否使用斜體？ (y / n)", "n");
-    var myposX = prompt("X Coord.", "0");
-    var myposY = prompt("Y Coord.", "0");
+    var myFontSize = prompt("Font Size (pixel): ", "30");
+    var myFontStyle = prompt("Font type code: (0 = Arial,  1 = sans-serif,  2 = serif,  3 = Verdana)", "0");
+    var myFontIta = prompt("italic (y / n)", "n");
+    var myposX = prompt("X position in canvas", "0");
+    var myposY = prompt("Y position in canvas", "0");
 
     if (myFontStyle == "0")
         myFontStyle = " Arial";
@@ -193,29 +193,25 @@ function addTxt() {
 
     ctx.font = myFontIta + myFontSize + "px" + myFontStyle;
 
-    // 拍照(未畫前)(Un/ReDo用)
+    //Save current canvas content before drawing (for Un/Re-do)
     if (imgData.length == 0) {
         imgPtr++;
         imgData[imgPtr] = ctx.getImageData(0, 0, 800, 600);
     }
 
     if (myTxt == "" || myTxt == null) {
-        // 沒輸入時的狀態
+        //If nothing input
     }
     else
         ctx.fillText(myTxt, myposX, Number(myposY) + 30);
 
-    // 拍照(Un/ReDo用)
+    //Save current canvas content (for Un/Re-do)
     imgPtr++;
     imgData[imgPtr] = ctx.getImageData(0, 0, 800, 600);
     imgData.splice(imgPtr + 1, imgData.length - imgPtr + 1);
 }
 
-
-// Cursor icon (10%)
-
-
-// Canvas重置
+//Reset entire canvas
 function clearMyCanvas() {
     var prevcolor2 = ctx.fillStyle;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -225,7 +221,7 @@ function clearMyCanvas() {
     imgData = [];
 }
 
-// Circle, rectangle and triangle
+//Change the cursor according to the tools selected
 function paintShape(type) {
     brushType = type;
     eraser_flag = 0;
@@ -248,7 +244,7 @@ function paintShape(type) {
     }
 }
 
-// Un/Re-do button
+//Un-do to the previous canvas content
 function Undo() {
     if (imgPtr > 0) {
         imgPtr--;
@@ -256,6 +252,8 @@ function Undo() {
 
     }
 }
+
+//Re-do to the latter canvas content
 function Redo() {
     if (imgPtr < imgData.length - 1) {
         imgPtr++;
@@ -263,12 +261,12 @@ function Redo() {
     }
 }
 
-// Image tool (5%)
+//Image tool
 function myImage() {
-
+    //Blank
 }
 
-// Canvas下載
+//Download canvas as JPG file
 function imgSave() {
     var link = document.createElement('a');
     link.download = "picture.png";
